be6d85d82b498ae342e116c5b8e4b9f427acece3
Arpit16012008 <arpit808800@gmail.com>
2026-09-19 15:28:04 +0530

    Add task model and migration

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -11,6 +11,7 @@ from app.models.club import Club
 from app.models.club_member import ClubMember
 from app.models.event import Event
 from app.models.registration import EventRegistration
+from app.models.task import Task
 
 config = context.config
 
